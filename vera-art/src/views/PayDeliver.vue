b76d43208c21fab50acb2de393b234a8f415d102
Maxim P <raiyin@ya.ru>
2023-05-06 09:10:46 +0300

    Payment translated

--- vera-art/src/views/PayDeliver.vue
+++ vera-art/src/views/PayDeliver.vue
@@ -1,14 +1,19 @@
 <script>
-export default {};
+import { useI18n } from 'vue-i18n';
+export default {
+    setup() {
+        const { t } = useI18n({ useScope: 'global' });
+    },
+};
 </script>
 
 <template>
     <section class="container paydelivery">
-        <p>В данный момент мой магазин работает по упрощенной схеме.</p>
         <p>
-            Вы связываетесь со мной любым удобным для вас способом, мы договариваемся о
-            картине и цене, и после предварительной полной оплаты картины и пересылки, я
-            отправляю вам картину.
+            {{ this.$t('payment.p_one') }}
+        </p>
+        <p>
+            {{ this.$t('payment.p_two') }}
         </p>
     </section>
 </template>
