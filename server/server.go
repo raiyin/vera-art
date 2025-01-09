b1dfb5886256022587b2cd83b8692e69d5564337
Maxim P. <raiyin@ya.ru>
2025-01-09 23:11:44 +0300

    VS-90 Make query sale

--- server/server.go
+++ server/server.go
@@ -1,47 +1,147 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
+	"log"
+	"math/rand"
 	"net/http"
-	"os"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	_ "github.com/mattn/go-sqlite3"
 )
 
-type Part struct {
-	NameRU     string `json:"name_ru"`
-	NameEN     string `json:"name_en"`
-	BaseRU     string `json:"base_ru"`
-	BaseEN     string `json:"base_en"`
-	MaterialRU string `json:"material_ru"`
-	MaterialEN string `json:"material_en"`
-	Size       string `json:"size"`
+type Book struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
 }
 
+type Author struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+var books []Book
+
 type Sale struct {
-	ID         string `json:"id"`
-	Dir        string `json:"dir"`
-	NameRu     string `json:"name_ru"`
-	NameEn     string `json:"name_en"`
-	Width      string `json:"width"`
-	Height     string `json:"height"`
-	BaseRu     string `json:"base_ru"`
-	BaseEn     string `json:"base_en"`
-	MaterialRu string `json:"material_ru"`
-	MaterialEn string `json:"material_en"`
-	Size       string `json:"size"`
-	Year       int    `json:"year"`
-	Price      int    `json:"price"`
-	Parts      []Part `json:"parts"`
+	Id     int    `json:"id"`
+	Dir    string `json:"dir"`
+	Width  string `json:"width"`
+	Height string `json:"height"`
+	Year   int    `json:"year"`
+	Price  int    `json:"price"`
+	NameRu string `json:"name_ru"`
+	NameEn string `json:"name_en"`
+	BaseId string `json:"base_id"`
+	StrId  string `json:"str_id"`
+}
+
+func sayhello(w http.ResponseWriter, r *http.Request) {
+	type ResponseId struct {
+		Id int `json:"id"`
+	}
+
+	js, err := json.Marshal(ResponseId{Id: rand.Int()})
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+func getSales(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("sqlite3", "db.sqlite")
+	if err != nil {
+		panic(err)
+	}
+	rows, err := db.Query("select * from sale")
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+	sales := []Sale{}
+	for rows.Next() {
+		p := Sale{}
+		err := rows.Scan(&p.Id, &p.Dir, &p.Width, &p.Height, &p.Year, &p.Price, &p.NameRu, &p.NameEn, &p.BaseId, &p.StrId)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		sales = append(sales, p)
+	}
+	for _, p := range sales {
+		fmt.Println(p.Id, p.NameRu, p.BaseId, p.Price)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(sales)
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range books {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Book{})
+}
+
+func createBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	book.ID = strconv.Itoa(rand.Intn(1000000))
+	books = append(books, book)
+	json.NewEncoder(w).Encode(book)
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			var book Book
+			_ = json.NewDecoder(r.Body).Decode(&book)
+			book.ID = params["id"]
+			books = append(books, book)
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(books)
+}
+
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(books)
 }
 
 func main() {
 
-	jsonFile, err := os.Open("db.json")
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World!")
-	})
-	http.ListenAndServe(":80", nil)
+	r := mux.NewRouter()
+	// books = append(books, Book{ID: "1", Title: "Война и Мир", Author: &Author{Firstname: "Лев", Lastname: "Толстой"}})
+	// books = append(books, Book{ID: "2", Title: "Преступление и наказание", Author: &Author{Firstname: "Фёдор", Lastname: "Достоевский"}})
+	r.HandleFunc("/sales", getSales).Methods("GET")
+	r.HandleFunc("/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/books", createBook).Methods("POST")
+	r.HandleFunc("/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/books/{id}", deleteBook).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
